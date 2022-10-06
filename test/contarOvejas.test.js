9e2a93a757dc6b5a322c3ad80d2df30af39e3229
Pablo Pedraza <wensel84@gmail.com>
2022-10-06 14:49:11 -0300

    agrego test ovejas

---
+++ test/contarOvejas.test.js
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest'
+
+const contarOvejas = (ovejas) => {
+  // aquí tu magia
+  const expresionA = /[a+]/gi
+  const expresionB = /[n+]/gi
+  return ovejas.filter(
+    (oveja) =>
+      oveja.color === 'rojo' &&
+      oveja.name.match(expresionA) &&
+      oveja.name.match(expresionB)
+  )
+}
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo' },
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo' }
+]
+
+describe('contarOvejas', () => {
+  it('should be a function', () => {
+    expect(typeof contarOvejas).toBe('function')
+  })
+
+  it('should return an array', () => {
+    expect(contarOvejas(ovejas)).toBeInstanceOf(Array)
+  })
+
+  it('should return an array with 2 objects', () => {
+    expect(contarOvejas(ovejas).length).toBe(2)
+  })
+})
